6d3e2005f0d17ff2b11019dd48a10b2afc4f4457
Samuel Muli <samuelmuli95@gmail.com>
2023-09-27 12:25:41 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 // Get a reference to the task input element.
+const taskInput = document.querySelector('#task-input')
 
 // Get a reference to the add task button element.
+const addTaskButton = document.querySelector('#add-task-button')
 
 // Get a reference to the task list element.
+const taskList = document.querySelector('#task-list')
 
 // Add an event listener to the add task button element.
-
-// Get the value of the task input element.
-
-// Create a new task list item element.
-
-// Add the task text to the task list item element.
-
-// Add the task list item element to the task list element.
-
-// Clear the task input element.
+addTaskButton.addEventListener('Click', () => {
+    // Get the value of the task input element.
+    const task = taskInput.value
+    // Create a new task list item element.
+    const taskItem = document.createElement('li')
+    // Add the task text to the task list item element.
+    taskItem.textContent = task
+    // Add the task list item element to the task list element.
+    taskList.appendChild(taskItem)
+    // Clear the task input element.
+    taskInput.value = ''
+})
 
 // Add an event listener to the task list element.
-
-// Get the target element of the event.
-
-// If the target element is a task list item element, toggle its completed class.
-
-
+taskList.addEventListener('click', (e) => {
+    // Get the target element of the event.
+    const target = e.target
+    // If the target element is a task list item element, toggle its completed class.
+    if(target.tagName === 'LI'){
+        target.classList.toggle('completed')
+    }
+})
